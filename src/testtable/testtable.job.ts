f817112e09c465b2bac60b221d308d353c3874ac
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-05-21 23:01:22 +1200

    Fix bug

--- src/testtable/testtable.job.ts
+++ src/testtable/testtable.job.ts
@@ -2,6 +2,6 @@ import { CronJob } from "cron";
 import { postTestData } from "./testtable.repository";
 
 export const postDataWithCron = new CronJob(`0 * * * *`, async () => {
-  const testData = `Cronjob at time:${new Date().toString}`;
+  const testData = `Cronjob at time:${new Date().toString()}`;
   await postTestData(testData);
 });
