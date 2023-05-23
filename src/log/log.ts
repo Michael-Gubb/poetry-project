71e960277d230c34af9e1bf33f872348c9ecadb7
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-05-23 12:07:09 +1200

    Add JSDoc

--- src/log/log.ts
+++ src/log/log.ts
@@ -1,7 +1,9 @@
+/** Logs to console*/
 export function logGetTestData(queryResult: any[]) {
   console.log(`Getting data of size ${queryResult.length}`);
 }
 
+/** Logs to console*/
 export function logPostTestData(id: string, data: string) {
   console.log(`Posting: ${id}, ${data}`);
 }
--- src/repository/getTime.ts
+++ src/repository/getTime.ts
@@ -1,5 +1,6 @@
 import pool from "../db";
 
+/** Returns current time in DB using SELECT NOW() */
 export async function getTime() {
   const { rows } = await pool.query("SELECT NOW();");
   return rows[0].now as string;
