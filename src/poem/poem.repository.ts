19af0e0b56b35e694044effd7680fa532c9678a5
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-05-29 19:24:43 +1200

    Add postpoem

---
+++ src/poem/poem.repository.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from "uuid";
+import pool from "../db";
+
+/**
+ * Returns the contents of poem table
+ */
+export async function getPoems() {
+  const getPoemsQuery = `SELECT * FROM poem;`;
+  const { rows } = await pool.query(getPoemsQuery);
+  //logGetTestData(rows);
+  return rows as Poem[];
+}
+/**
+ * Inserts into testtable the provided data with a newly generated UUID primary key
+ * @param data {string} data to insert into new row
+ */
+export async function postPoem(poemText: string, poemTopics: string[]) {
+  const getTestDataQuery = `INSERT INTO poem (poem_id,poem_text,poem_topics,poem_date) VALUES ($1,$2,$3,$4);`;
+  const queryUuid = uuidv4();
+  const currentDate = new Date();
+  const currentDateString = currentDate.toLocaleString("en-GB", {
+    timeZone: `Pacific/Auckland`,
+  });
+  //logPostTestData(queryUuid, data);
+  const { rows } = await pool.query(getTestDataQuery, [
+    queryUuid,
+    poemText,
+    poemTopics,
+    currentDateString,
+  ]);
+  return rows;
+}
