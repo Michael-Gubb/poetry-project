717e30a037799c390821e0887790251f1e44a06b
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-05-30 11:27:29 +1200

    Refactor cronjobs

--- src/cron/cronjobs.ts
+++ src/cron/cronjobs.ts
@@ -1,6 +1,6 @@
 import { Cron, CronOptions } from "croner";
 import { postTestData } from "../testtable/testtable.repository";
-import { getPoemTopics } from "../utils/poem.utils";
+import { getPoemTopics, getPoemGenre } from "../utils/poem.utils";
 import { askForPoem } from "../openai/openai.request";
 import { postPoem } from "../poem/poem.repository";
 
@@ -17,13 +17,22 @@ export const postDataWithCron = new Cron(everyHour, cronOptions, async () => {
 });
 
 /** Logs a message every minute */
-export const testCronJob = new Cron(everyMinute, cronOptions, async () => {
-  console.log(`Test cronjob at time:${new Date().toString()}`);
-});
+export const testCronJob = new Cron(everyMinute, cronOptions, testJob);
 
 /** Generates a new poem and posts to poem table every hour*/
-export const openAIRequest = new Cron(everyHour, cronOptions, async () => {
+export const hourlyGeneratePoem = new Cron(
+  everyHour,
+  cronOptions,
+  generateAndPostPoem
+);
+
+async function generateAndPostPoem() {
   const poemTopics = getPoemTopics();
+  const poemGenre = getPoemGenre();
   const poemResponse = await askForPoem(poemTopics);
-  await postPoem(poemResponse.content, poemTopics);
-});
+  await postPoem(poemResponse.content, poemTopics, poemGenre);
+}
+
+async function testJob() {
+  console.log(`Test cronjob at time:${new Date().toString()}`);
+}
