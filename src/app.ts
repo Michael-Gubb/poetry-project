0191856498e5dc9a3f11acb7eb59b453fd28b4b7
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-06-03 13:14:40 +1200

    Cleanup and add model used

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,8 @@ dotenv.config();
 import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
-import { testrouter } from "./testtable/testtable.router";
 import { poemRouter } from "./poem/poem.router";
 import { adminRouter } from "./admin/admin.router";
-import * as cronJobs from "./cron/cron.controller";
 import appSetup from "./app.setup";
 
 export const app = express();
@@ -19,11 +17,7 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.use("/admin", adminRouter);
-
-app.use("/api/test", testrouter);
-
 app.use("/api/poems", poemRouter);
-
 app.get("/api/healthcheck", (req, res) => {
   res.send(`Server running`);
 });
